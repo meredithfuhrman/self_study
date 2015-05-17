192364df0cff0d1b9ec5b1637090fcb8fe58cd0d
Meredith Fuhrman <meredith.fuhrman@gmail.com>
2015-05-17 18:11:17 -0400

    Next round of work

--- learn/ex16a.rb
+++ learn/ex16a.rb
@@ -6,11 +6,9 @@ puts "If you do want that, hit RETURN."
 
 $stdin.gets
 
-puts "Opening the file..."
+puts "Truncating the file!"
 target = open(filename, 'w')
 
-puts "Truncating the file. Goodbye!"
-target.truncate(0)
 
 puts "Now I'm going to ask you for three lines."
 
@@ -22,12 +20,7 @@ print "line 3: "
 line3 = $stdin.gets.chomp
 
 puts "I'm going to write these to the file."
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write("#{line1}\n#{line2}\n#{line3}\n")
 
 puts "And finally, we close it."
 target.close
